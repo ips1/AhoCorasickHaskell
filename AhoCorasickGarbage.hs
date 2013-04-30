5e20d5991696b4efacdec88966ca04f03ba47e77
unknown <nando128@gmail.com>
2013-04-30 23:20:40 +0200

    Initial commit

---
+++ AhoCorasickGarbage.hs
@@ -0,0 +1,45 @@
+module AhoCorasickGarbage where
+
+import AhoCorasick
+
+setEdge :: Trie -> Trie -> Trie
+setEdge n (Node t s _) = Node t s n
+
+
+-- Returns a node where the word ends
+findNode :: Trie -> String -> Maybe Trie
+findNode n [] = Just n
+findNode (Node _ s _) (x:xs) = case (Map.lookup x s) of (Just k) -> findNode k xs
+                                                        (Nothing) -> Nothing
+
+-- Converts the trie into a list
+toList :: Trie -> [String]
+toList (Node [] s _) = foldr (++) [] (map (dfs "") (Map.toList s))
+toList (Node _ s _) = []:(foldr (++) [] (map (dfs "") (Map.toList s)))
+
+dfs :: String -> (Char, Trie) -> [String]
+dfs acc (c, (Node [] s _)) = (foldr (++) [] (map (dfs acc2) (Map.toList s)))
+  where acc2 = c:acc
+dfs acc (c, (Node _ s _)) = (reverse acc2):(foldr (++) [] (map (dfs acc2) (Map.toList s)))
+  where acc2 = c:acc
+
+
+-- Replaces a node with a new one
+replaceNode	:: Trie -> String -> Trie -> Trie
+replaceNode n [] n1 = n1
+replaceNode (Node t s b) (x:xs) n1 = let node = (Map.lookup x s)
+                                     in case node of 
+                                          Just n -> Node t (Map.insert x (replaceNode n xs n1) s) b
+                                          Nothing -> Node t s b  
+
+-- Debug output --
+printTrie :: Trie -> [(Char, String)]
+printTrie (Node t m None) = foldr (++) [] (map printNode (Map.toList m))
+
+printNode :: (Char, Trie) -> [(Char, String)]
+printNode (c, (Node t m b)) = (c, printChildren b):(foldr (++) [] (map printNode (Map.toList m)))
+
+printChildren :: Trie -> String
+printChildren (Node t m b) = map fst (Map.toList m)
+printChildren None = []
+------------------
---
+++ main.hs
@@ -0,0 +1,39 @@
+module Main where
+
+import AhoCorasick
+import System.Exit
+import System.Directory  
+import System.IO
+
+ins :: Trie -> IO Trie
+ins t = do
+    word <- getLine
+    if null word 
+      then return t
+      else ins $ addString t word
+
+promptFile :: IO String
+promptFile = do
+    putStrLn "Soubor k prohledani:"
+    fileName <- getLine
+    fileExists <- doesFileExist fileName  
+    if fileExists  
+      then return fileName
+      else do putStrLn "Soubor neexistuje!"
+              putStrLn ""
+              promptFile  
+
+main = do
+    putStrLn ""
+    putStrLn "----------------------"
+    putStrLn "| StringFinder v 1.0 |"
+    putStrLn "----------------------"
+    putStrLn ""
+    putStrLn "Slova k vyhledani (po skonceni 2x odentrujte):"
+    t <- ins empty
+    fileName <- promptFile
+    contents <- readFile fileName
+    putStrLn ""
+    putStrLn "Vysledek hledani ve formatu (koncovy_index, slovo):"
+    mapM_ print (performSearch (makeBackEdges t) contents)
+    getLine
